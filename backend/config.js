cfaf0ddce19c31e9d5a298cbd2f13070cec659d3
desalegn kassa <desalegnashagrie@gmail.comgit config user.emailgit config user.emailgit config --global user.email desalegnashagrie@gmail.com>
2023-04-29 03:10:09 -0500

    fixed uri again

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb+srv://desalegnrender:render@cluster0.tzhazec.mongodb.net/test',
+    'mongo_URI': 'mongodb+srv://desalegnrender:render@cluster0.tzhazec.mongodb.net/test',
     
 }
 /*./bin/www */
